fd2ab233d5bc672bc5050c2526fbe0678f7d61f4
BerrocalG <guadalupe.berrocal@usil.pe>
2025-04-22 22:34:44 -0500

    2 display tratando de unir matriz

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#define F_CPU 16000000UL  
+/*#define F_CPU 16000000UL  
 #include <avr/io.h>  
 #include <util/delay.h>
 
@@ -13,4 +13,60 @@ int main(void) {
   char cuenta=0;
   char boton= 0;
 
+  while(1){
+  
+  }
+
 }
+*/
+
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void) {
+    DDRB |= 0x0F;   // decodificador
+    DDRC |= 0x07;   // prendido apagado display
+
+    DDRD |= 0x0F;  // fila salidas
+    DDRD&=~0XF0;     // colunas entrada
+
+    PORTD |= 0x01;  // Habilitar pull-ups en PB2 y PB3
+    PORTD&=~ 0X03;
+
+    //PORTC|=0X07;
+
+    char cuenta = 0;
+    char pulsadores = 0;
+
+    while (1) {
+
+        PORTC &= ~0x01;     // par apagar el display de decenas
+        PORTC |= 0x02;      // para prender el display de unidades
+        PORTB = (PORTB & 0xF0) | ((cuenta % 10) << 4);  // para "desplazar" las unidades a losp ines del deco
+        _delay_ms(8); //display
+
+        // Mostrar decenas
+        PORTC &= ~0x02;     // para apagar el display de unidades
+        PORTC |= 0x01;      // para prender e display de decenas
+        PORTB = (PORTB & 0xF0) | ((cuenta / 10) << 4);  // para desplazar las decenas a los pines del deco
+        _delay_ms(8);
+
+        // Leer botones
+        pulsadores = (PIND & 0x31); // para indicar uso de los pines pb10 pb11
+
+        if (pulsadores == 0x11) {  // boton para subir
+            cuenta++; //aumenta 1 unidad
+            if (cuenta > 99)
+            cuenta = 99; //la suma no va sobre pasar los 99
+            _delay_ms(200);//boton
+
+
+        } else if (pulsadores == 0x21) {  // boton para bajar
+            if (cuenta > 0)
+            cuenta--;
+            _delay_ms(200);
+        }
+    }
+}
+
